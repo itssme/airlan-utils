1cd7c265269f9aa5501348abe45f0658c29e66cc
Joel Klimont <joel.klimont@gmail.com>
2023-04-10 23:41:56 +0200

    fixed old db requests

--- db_stats.py
+++ db_stats.py
@@ -11,9 +11,10 @@ from utils.stat_events import Events
 def count_event_type(event: Events) -> int:
     with psycopg2.connect(
             host=os.getenv("DB_HOST", "db"),
-            database="postgres",
-            user="postgres",
-            password=os.getenv("DB_PASSWORD", "pass")) as conn:
+            port=int(os.getenv('POSTGRES_DB_PORT', '5432')),
+            database=os.getenv('POSTGRES_DB', 'postgres'),
+            user=os.getenv('POSTGRES_USER', 'postgres'),
+            password=os.getenv("POSTGRES_PASSWORD", "pass")) as conn:
         with conn.cursor() as cursor:
             cursor.execute("select count(*) from stats where type = %s", (event.value,))
             return cursor.fetchall()[0][0]
@@ -22,9 +23,10 @@ def count_event_type(event: Events) -> int:
 def player_with_most(event: Events) -> List[Tuple[str, int]]:
     with psycopg2.connect(
             host=os.getenv("DB_HOST", "db"),
-            database="postgres",
-            user="postgres",
-            password=os.getenv("DB_PASSWORD", "pass")) as conn:
+            port=int(os.getenv('POSTGRES_DB_PORT', '5432')),
+            database=os.getenv('POSTGRES_DB', 'postgres'),
+            user=os.getenv('POSTGRES_USER', 'postgres'),
+            password=os.getenv("POSTGRES_PASSWORD", "pass")) as conn:
         with conn.cursor() as cursor:
             cursor.execute(
                 "select player.name, count(*) from stats join player on stats.player = player.id where stats.type = %s group by player.name order by count(*) desc, player.name limit 5",
