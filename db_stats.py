9a8ba8451a283b9e6de07d7a06e6ecb41179a330
Joel Klimont <itssme3000@gmail.com>
2023-04-13 20:02:26 +0200

    speedup in /stats

--- db_stats.py
+++ db_stats.py
@@ -4,20 +4,13 @@ from typing import Tuple, List
 
 import psycopg2
 
+from utils import db_models
 from utils.db import DbObjImpl, Stats
 from utils.stat_events import Events
 
 
 def count_event_type(event: Events) -> int:
-    with psycopg2.connect(
-            host=os.getenv("POSTGRES_DB_HOST", "db"),
-            port=int(os.getenv('POSTGRES_DB_PORT', '5432')),
-            database=os.getenv('POSTGRES_DB', 'postgres'),
-            user=os.getenv('POSTGRES_USER', 'postgres'),
-            password=os.getenv("POSTGRES_PASSWORD", "pass")) as conn:
-        with conn.cursor() as cursor:
-            cursor.execute("select count(*) from stats where type = %s", (event.value,))
-            return cursor.fetchall()[0][0]
+    return db_models.Stats.select().where(db_models.Stats.type == event.value).count()
 
 
 def player_with_most(event: Events) -> List[Tuple[str, int]]:
