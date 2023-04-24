ebeaf0efc491fd122d6cff86f9375468c6af60ae
Joel Klimont <joel.klimont@gmail.com>
2023-04-24 18:25:49 +0200

    improved stats request (90ms to 15ms) fixed bug where an old db object was used

--- db_stats.py
+++ db_stats.py
@@ -1,11 +1,5 @@
-import logging
-import os
 from typing import Tuple, List
-
-import psycopg2
-
 from utils import db_models
-from utils.db import DbObjImpl, Stats
 from utils.stat_events import Events
 
 
@@ -14,17 +8,8 @@ def count_event_type(event: Events) -> int:
 
 
 def player_with_most(event: Events) -> List[Tuple[str, int]]:
-    with psycopg2.connect(
-            host=os.getenv("POSTGRES_DB_HOST", "db"),
-            port=int(os.getenv('POSTGRES_DB_PORT', '5432')),
-            database=os.getenv('POSTGRES_DB', 'postgres'),
-            user=os.getenv('POSTGRES_USER', 'postgres'),
-            password=os.getenv("POSTGRES_PASSWORD", "pass")) as conn:
-        with conn.cursor() as cursor:
-            cursor.execute(
-                "select player.name, count(*) from stats join player on stats.player = player.id where stats.type = %s group by player.name order by count(*) desc, player.name limit 5",
-                (event.value,))
-            result = cursor.fetchall()
-            if len(result) == 0:
-                return []
-            return result
+    cursor = db_models.database.execute_sql(
+        "select player.name, count(*) from stats join player on stats.player = player.id where stats.type = %s group by player.name order by count(*) desc, player.name limit 5",
+        (event.value,))
+    result: List[Tuple[str, int]] = list(cursor.fetchall())
+    return result
