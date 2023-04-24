475516e197b283bf99bbf85b0b58684e510ea316
Joel Klimont <joel.klimont@gmail.com>
2023-04-24 17:31:57 +0200

    switched to production version of /stats

--- db.py
+++ db.py
@@ -26,111 +26,6 @@ def get_db(db_state=Depends(reset_db_state)):
             db_models.database.close()
 
 
-class DbObject(object):
-    def tuple(self):
-        return tuple(vars(self).values())
-
-    def to_json(self) -> dict:
-        return vars(self)
-
-    def __str__(self):
-        return str(self.to_json())
-
-    def __repr__(self):
-        return str(self.to_json())
-
-    def insert_into_db(self):
-        with psycopg2.connect(
-                host=os.getenv("POSTGRES_DB_HOST", "db"),
-                port=int(os.getenv('POSTGRES_DB_PORT', '5432')),
-                database=os.getenv('POSTGRES_DB', 'postgres'),
-                user=os.getenv('POSTGRES_USER', 'postgres'),
-                password=os.getenv("POSTGRES_PASSWORD", "pass")) as conn:
-            with conn.cursor() as cursor:
-                self.insert_into_db_with_cursor(cursor)
-
-    def insert_into_db_with_cursor(self, cursor):
-        stmt = f"insert into {self.__class__.__name__.lower()} ({', '.join(list(filter(lambda key: (key != 'id'), list(vars(self).keys()))))}) values ({', '.join(['%s' for elm in list(filter(lambda key: (key != 'id'), list(vars(self).keys())))])}) {'returning id' if 'id' in list(vars(self).keys()) else ''}"
-        values = tuple(
-            self.__getattribute__(elm) for elm in
-            list(filter(lambda key: (key != 'id'), list(vars(self).keys()))))
-        logging.info(f"Running query: {stmt}\nwith values: {values}")
-        cursor.execute(stmt, values)
-        if "id" in vars(self).keys():
-            self.__setattr__("id", cursor.fetchall()[0][0])
-
-    # WARNING: Currently only works if the object is using the 'id' attribute as primary key
-    def update_attribute(self, attr: str):
-        if "id" not in vars(self).keys():
-            raise NotImplemented("Cannot insert object that does not use 'id' as primary key")
-
-        with psycopg2.connect(
-                host=os.getenv("POSTGRES_DB_HOST", "db"),
-                port=int(os.getenv('POSTGRES_DB_PORT', '5432')),
-                database=os.getenv('POSTGRES_DB', 'postgres'),
-                user=os.getenv('POSTGRES_USER', 'postgres'),
-                password=os.getenv("POSTGRES_PASSWORD", "pass")) as conn:
-            with conn.cursor() as cursor:
-                self.update_attribute_with_cursor(cursor, attr)
-
-    def update_attribute_with_cursor(self, cursor, attr: str):
-        stmt = f"update {self.__class__.__name__.lower()} set {attr} = %s where id = %s"
-        values = (self.__getattribute__(attr), self.__getattribute__("id"))
-        logging.info(f"Running query: {stmt}\nwith values: {values}")
-        cursor.execute(stmt, values)
-
-
-class Server(DbObject):
-    def __init__(self, id: int = None, ip: str = "host.docker.internal", port: int = None, gslt_token: str = "",
-                 container_name: str = None, match: int = None):
-        self.id: int = id
-        self.ip: str = ip
-        self.port: int = port
-        self.gslt_token: str = gslt_token
-        self.container_name: str = container_name
-        self.match: int = match
-
-
-class Match(DbObject):
-    def __init__(self, id: int = None, matchid: str = "", name: str = "", team1: int = None, team2: int = None,
-                 best_out_of: int = None, number_in_map_series: int = 0, series_score_team1: int = 0,
-                 series_score_team2: int = 0, finished: int = 0):
-        self.id: int = id
-        self.matchid: str = matchid
-        self.name: str = name
-        self.team1: int = team1
-        self.team2: int = team2
-        self.best_out_of: int = best_out_of
-        self.number_in_map_series: int = number_in_map_series
-        self.series_score_team1: int = series_score_team1
-        self.series_score_team2: int = series_score_team2
-        self.finished: int = finished
-
-
-class Stats(DbObject):
-    def __init__(self, id: int = None, match: int = None, player: int = None, type: int = None):
-        self.id: int = id
-        self.match: int = match
-        self.player: int = player
-        self.type: int = type
-
-
-T = TypeVar("T", Server, Match, Stats)
-
-
-class DbObjImpl(Generic[T]):
-    def from_json(self, dict: dict) -> T:
-        obj = self.__orig_class__.__args__[0]()
-        for attr in vars(obj).keys():
-            if attr in dict.keys():
-                obj.__setattr__(attr, dict[attr])
-
-        return self.from_tuple(tuple(vars(obj).values()))
-
-    def from_tuple(self, tuple: tuple) -> T:
-        return self.__orig_class__.__args__[0](*tuple)
-
-
 def get_free_teams() -> List[db_models.Team]:
     all_teams = db_models.Team().select().where(db_models.Team.competing < 1)
     busy_teams = db_models.Team().select().join(db_models.Match, on=(
@@ -149,117 +44,18 @@ def get_player_by_steam_id(steam_id: str) -> db_models.Player:
     return db_models.Player().select().where(db_models.Player.steam_id == steam_id).first()
 
 
-def get_servers() -> List[Server]:
-    with psycopg2.connect(
-            host=os.getenv("POSTGRES_DB_HOST", "db"),
-            port=int(os.getenv('POSTGRES_DB_PORT', '5432')),
-            database=os.getenv('POSTGRES_DB', 'postgres'),
-            user=os.getenv('POSTGRES_USER', 'postgres'),
-            password=os.getenv("POSTGRES_PASSWORD", "pass")) as conn:
-        with conn.cursor() as cursor:
-            cursor.execute("select * from server")
-            servers = cursor.fetchall()
-            return [DbObjImpl[Server]().from_tuple(server) for server in servers]
+def get_match_by_serverid(server_id: int) -> db_models.Match:
+    return db_models.Match.select().join(db_models.Server, on=(db_models.Match.id == db_models.Server.match)).where(
+        db_models.Server.id == server_id).first()
 
 
-def get_server_by_id(server_id: int) -> Server:
-    with psycopg2.connect(
-            host=os.getenv("POSTGRES_DB_HOST", "db"),
-            port=int(os.getenv('POSTGRES_DB_PORT', '5432')),
-            database=os.getenv('POSTGRES_DB', 'postgres'),
-            user=os.getenv('POSTGRES_USER', 'postgres'),
-            password=os.getenv("POSTGRES_PASSWORD", "pass")) as conn:
-        with conn.cursor() as cursor:
-            cursor.execute("select * from server where id = %s", (server_id,))
-            return DbObjImpl[Server]().from_tuple(cursor.fetchall()[0])
-
-
-def delete_server(server_id: int):
-    with psycopg2.connect(
-            host=os.getenv("POSTGRES_DB_HOST", "db"),
-            port=int(os.getenv('POSTGRES_DB_PORT', '5432')),
-            database=os.getenv('POSTGRES_DB', 'postgres'),
-            user=os.getenv('POSTGRES_USER', 'postgres'),
-            password=os.getenv("POSTGRES_PASSWORD", "pass")) as conn:
-        with conn.cursor() as cursor:
-            cursor.execute("delete from server where id = %s", (server_id,))
-
-
-def get_match_by_id(match_id: int) -> Match:
-    with psycopg2.connect(
-            host=os.getenv("POSTGRES_DB_HOST", "db"),
-            port=int(os.getenv('POSTGRES_DB_PORT', '5432')),
-            database=os.getenv('POSTGRES_DB', 'postgres'),
-            user=os.getenv('POSTGRES_USER', 'postgres'),
-            password=os.getenv("POSTGRES_PASSWORD", "pass")) as conn:
-        with conn.cursor() as cursor:
-            cursor.execute("select * from match where id = %s", (match_id,))
-            return DbObjImpl[Match]().from_tuple(cursor.fetchall()[0])
-
-
-def get_match_by_matchid(matchid: str) -> Match:
-    with psycopg2.connect(
-            host=os.getenv("POSTGRES_DB_HOST", "db"),
-            port=int(os.getenv('POSTGRES_DB_PORT', '5432')),
-            database=os.getenv('POSTGRES_DB', 'postgres'),
-            user=os.getenv('POSTGRES_USER', 'postgres'),
-            password=os.getenv("POSTGRES_PASSWORD", "pass")) as conn:
-        with conn.cursor() as cursor:
-            cursor.execute("select * from match where matchid = %s", (matchid,))
-            return DbObjImpl[Match]().from_tuple(cursor.fetchall()[0])
-
-
-def get_match_by_serverid(server_id: int) -> Match:
-    with psycopg2.connect(
-            host=os.getenv("POSTGRES_DB_HOST", "db"),
-            port=int(os.getenv('POSTGRES_DB_PORT', '5432')),
-            database=os.getenv('POSTGRES_DB', 'postgres'),
-            user=os.getenv('POSTGRES_USER', 'postgres'),
-            password=os.getenv("POSTGRES_PASSWORD", "pass")) as conn:
-        with conn.cursor() as cursor:
-            cursor.execute("select match.* from match join server on server.match = match.id where server.id = %s",
-                           (server_id,))
-            return DbObjImpl[Match]().from_tuple(cursor.fetchall()[0])
-
-
-def insert_match(match: Match):
-    with psycopg2.connect(
-            host=os.getenv("POSTGRES_DB_HOST", "db"),
-            port=int(os.getenv('POSTGRES_DB_PORT', '5432')),
-            database=os.getenv('POSTGRES_DB', 'postgres'),
-            user=os.getenv('POSTGRES_USER', 'postgres'),
-            password=os.getenv("POSTGRES_PASSWORD", "pass")) as conn:
-        with conn.cursor() as cursor:
-            match.insert_into_db_with_cursor(cursor)
-
-
-def get_server_for_match(matchid: str) -> Server:
-    with psycopg2.connect(
-            host=os.getenv("POSTGRES_DB_HOST", "db"),
-            port=int(os.getenv('POSTGRES_DB_PORT', '5432')),
-            database=os.getenv('POSTGRES_DB', 'postgres'),
-            user=os.getenv('POSTGRES_USER', 'postgres'),
-            password=os.getenv("POSTGRES_PASSWORD", "pass")) as conn:
-        with conn.cursor() as cursor:
-            cursor.execute("select server.* from server join match on server.match = match.id where match.matchid = %s",
-                           (matchid,))
-            return DbObjImpl[Server]().from_tuple(cursor.fetchall()[0])
-
-
-def get_hosts() -> List[str]:
-    with psycopg2.connect(
-            host=os.getenv("POSTGRES_DB_HOST", "db"),
-            port=int(os.getenv('POSTGRES_DB_PORT', '5432')),
-            database=os.getenv('POSTGRES_DB', 'postgres'),
-            user=os.getenv('POSTGRES_USER', 'postgres'),
-            password=os.getenv("POSTGRES_PASSWORD", "pass")) as conn:
-        with conn.cursor() as cursor:
-            cursor.execute("select * from host")
-            server_tuple_list = cursor.fetchall()
-            return [host[0] for host in server_tuple_list]
+def get_server_for_match(matchid: str) -> db_models.Server:
+    return db_models.Server.select().join(db_models.Match, on=(db_models.Server.match == db_models.Match.id)).where(
+        db_models.Match.matchid == matchid).first()
 
 
 def create_account(username: str, password: str):
+    # TODO: refactor random string generation, create function and use better randomness (also see password reset token)
     account = db_models.Account.create(username=username, password=password, verification_code="".join(
         [random.choice([chr(random.randint(48, 57)), chr(random.randint(65, 90))]) for _ in range(0, 60)]))
 
