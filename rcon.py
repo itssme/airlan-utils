f33cd94cd78b961c3289aa43b909e6162eed856d
Joel Klimont <joel.klimont@gmail.com>
2023-05-14 18:57:33 +0200

    added new analytics page to show infos about running games fixed multiple errors regarding stats events

--- rcon.py
+++ rcon.py
@@ -7,7 +7,7 @@ from srcds.rcon import RconConnection
 
 class RCON(RconConnection):
     def __init__(self, server, port=27015, password=os.getenv("RCON_PASSWORD", "pass"), single_packet_mode=False):
-        super().__init__(server, port=port, password=password, single_packet_mode=single_packet_mode)
+        super().__init__(str(server), port=port, password=password, single_packet_mode=single_packet_mode)
 
     def __enter__(self):
         return self
