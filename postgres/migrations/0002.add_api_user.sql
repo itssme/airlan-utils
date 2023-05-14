9bcdfa087d09dd9f77fcf79b29f34f8749354a16
Joel Klimont <joel.klimont@gmail.com>
2023-05-14 23:38:06 +0200

    if user with the name api already exists, ignore the migration step

--- postgres/migrations/0002.add_api_user.sql
+++ postgres/migrations/0002.add_api_user.sql
@@ -1,2 +1,3 @@
 insert into "account" ("username", "password", "role", "verified", "verification_code")
-values ('api', '', 'admin', 1, '');
+values ('api', '', 'admin', 1, '')
+on conflict do nothing;
